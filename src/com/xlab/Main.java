b42f8ad2416ed29e79926c65b4a066816221f768
kiba-zwei <xingbinkiba@163.com>
2020-02-15 17:46:52 +0800

    代码合入

--- src/com/xlab/Main.java
+++ src/com/xlab/Main.java
@@ -4,12 +4,12 @@ import java.io.IOException;
 import java.util.List;
 
 public class Main {
-    private static final int TIME_INTERVAL = 1 * 1000;
+    private static final String TIP_PATH = "README.txt";
 
     public static void main(String[] args) {
         tips();
         try {
-            List<VisitorConfig> configs = VisitorConfig.createConfiges(FileParser.getInstance().read());
+            List<VisitorConfig> configs = VisitorConfig.createConfiges(FileParser.parseConfig());
             for (VisitorConfig vc : configs) {
                 visit(vc);
             }
@@ -19,24 +19,15 @@ public class Main {
     }
 
     private static void tips() {
-        System.out.println("*******************************************************************************************************************");
-        System.out.println("受某同学之托，搞一个刷文章阅读量的工具，具体使用方法如下：\n" +
-                "1、下载Java JDK 11 并配置环境变量，见： https://blog.csdn.net/kiba_zwei/article/details/103645213；\n" +
-                "2、在config.txt中填入刷新次数与目标网页地址，第一行为次数，第二行为地址，第三行为次数，第四行为地址，以此类推；\n" +
-                "3、点击start.bat开始刷流量；\n" +
-                "4、本工具实现方式为模拟点击网页的方式进行流量刷新，使用过程中会反复弹出网页，影响电脑使用，请这段时间暂离电脑；\n" +
-                "5、刷新时间为：10*N s, N为刷新次数；\n" +
-                "6、有使用问题欢迎联系：xingbinkiba@163.com");
-        System.out.println("示例：");
-        System.out.println("10");
-        System.out.println("www.baidu.com");
-        System.out.println("*******************************************************************************************************************");
+        for (String str : FileParser.read(TIP_PATH)) {
+            System.out.println(str);
+        }
     }
 
     private static void visit(VisitorConfig vc) throws IOException, InterruptedException {
-        for (int i = 1; i <= vc.num; i++) {
-            CmdProxy.getInstance().openWeb(vc.url);
-            Thread.sleep(TIME_INTERVAL);
+        for (int i = 1; i <= vc.getNum(); i++) {
+            CmdProxy.getInstance().openWeb(vc.getUrl());
+            Thread.sleep(vc.getInterval());
             if (i % 10 == 0) {
                 CmdProxy.getInstance().closeWeb();
             }
--- src/com/xlab/VisitorConfig.java
+++ src/com/xlab/VisitorConfig.java
@@ -1,21 +1,35 @@
 package com.xlab;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class VisitorConfig {
     private static final int MIN_SIZE_OF_CONFIG = 2;
-    int num;
-    String url;
+    private static final int ONE_SECOND = 1000;
+    private static final int DEFAULT_NUM = 10;
+    private static final int DEFAULT_INTERVAL = 10;
+    private int num;
+    private String url;
+    private int interval = 10;
 
     public VisitorConfig(int num, String url) {
         this.num = num;
         this.url = url;
     }
 
+    public VisitorConfig(int num, String url, int interval) {
+        this.num = num;
+        this.url = url;
+        this.interval = interval;
+    }
+
     public static List<VisitorConfig> createConfiges(List<String> config) throws Exception {
         if (config == null || config.size() % MIN_SIZE_OF_CONFIG != 0) {
-            throwException("Invaild config");
+            throwException("Invaild config.txt");
         }
         List<VisitorConfig> configs = new ArrayList<>();
         int num = 0;
@@ -31,10 +45,61 @@ public class VisitorConfig {
                     configs.add(new VisitorConfig(num, url));
                     break;
                 default:
-                    throwException("wrong type of config");
+                    throwException("wrong type of config.txt");
             }
         }
         return configs;
+    }
+
+    public static List<VisitorConfig> createConfiges(String configs) throws Exception {
+        if (configs == null || configs.length() == 0) {
+            return null;
+        }
+        List<VisitorConfig> visitorConfigs = new ArrayList<>();
+        JSONObject jobj = JSON.parseObject(configs);
+        JSONArray configList = jobj.getJSONArray("config");
+
+        for (int i = 0; i < configList.size(); i++) {
+            JSONObject jsonObject = configList.getJSONObject(i);
+            try {
+                int count = jsonObject.getInteger("count");
+                int interval = jsonObject.getInteger("time_interval");
+                String url = jsonObject.getString("url");
+                VisitorConfig vc = new VisitorConfig(count, url, interval);
+                System.out.println(vc);
+                visitorConfigs.add(vc);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return visitorConfigs;
+    }
+
+    public int getNum() {
+        if (num < 0) {
+            return DEFAULT_NUM;
+        }
+        return num;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getInterval() {
+        if (interval < 0) {
+            return DEFAULT_INTERVAL * ONE_SECOND;
+        }
+        return interval * ONE_SECOND;
+    }
+
+    @Override
+    public String toString() {
+        return "VisitorConfig{" +
+                "num=" + num +
+                ", url='" + url + '\'' +
+                ", interval=" + interval +
+                '}';
     }
 
     private static final void throwException(String msg) throws Exception {
