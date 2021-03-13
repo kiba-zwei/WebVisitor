b623d32a3fc17886a1e8526080071e1d39e76b2d
kiba-zwei <xingbinkiba@163.com>
2021-03-13 10:20:18 +0800

    feat:增加随机访问逻辑

--- src/com/xlab/Main.java
+++ src/com/xlab/Main.java
@@ -2,6 +2,7 @@ package com.xlab;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
     private static final String TIP_PATH = "README.txt";
@@ -19,15 +20,14 @@ public class Main {
     }
 
     private static void tips() {
-        for (String str : FileParser.read(TIP_PATH)) {
-            System.out.println(str);
-        }
+        FileParser.read(TIP_PATH).stream().forEach(str -> System.out.println(str));
     }
 
     private static void visit(VisitorConfig vc) throws IOException, InterruptedException {
         for (int i = 1; i <= vc.getNum(); i++) {
             CmdProxy.getInstance().openWeb(vc.getUrl());
-            Thread.sleep(vc.getInterval());
+            int interval = new Random().nextInt(vc.getInterval());
+            Thread.sleep(interval == 0 ? vc.getInterval() : interval);
             if (i % 10 == 0) {
                 CmdProxy.getInstance().closeWeb();
             }
